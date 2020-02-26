53e034b1906f6d166f5d5edc490e4a7d11ae8ac8
Mohamed Adel <ov3rcontrol@Mohameds-iMac.local>
2020-02-26 21:00:58 +0200

    :sparkles: Add Global ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const serverConfig = config.get('server');
-
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //disableErrorMessages: true,
+      // forbidNonWhitelisted: true,
+      // whitelist: true,
+    }),
+  );
   if (process.env.NODE_ENV === 'development') {
     app.enableCors();
   } else {
